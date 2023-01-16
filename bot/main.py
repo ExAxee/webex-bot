83091a1b8459b1f5205b464a667d6d4a3d104280
ExAxee <dmr475@gmail.com>
2023-01-16 16:01:34 +0100

    add base message retrieval, add request function to make the code slimmer

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from urllib import response
 import requests
-import json
+import time
 
 # FOR REFERENCE ON WEBEX API: https://developer.webex.com/docs/api/v1/messages
 
@@ -20,13 +20,34 @@ HEADERS  = {
     "Content-Type": "application/json"
 }
 
+# Function declaratins
+def request(url: str, **kwargs: any) -> requests.Response:
+    return requests.get(
+        f"{url}?{'&'.join([key+'='+kwargs[key] for key in kwargs.keys()])}",
+        headers=HEADERS
+    )
+
 print("starting bot...")
 while True:
-    endpoint = BASE_URL + "rooms?sortBy=lastactivity"
-    response = requests.get(endpoint, headers=HEADERS)
+    recieve_endpoint = BASE_URL + "rooms"
+    message_endpoint = BASE_URL + "messages"
+    response = request(recieve_endpoint, sortBy="lastactivity")
 
     if response.status_code == 200:
+        update = response.json()
+
+        for item in update["items"]:
+            roomID = item["id"]
+            messages = request(message_endpoint, roomId=roomID)
+            mjson = messages.json()
+            
+            for message_item in mjson["items"]:
+                pass
         # TODO ADD FUNCTIONALITY
+        pass
+    elif response.status_code == 429: # too many requests
         pass
     else:
         raise ConnectionError(f"got response code: {response.status_code} with data {response.json()}")
+    
+    time.sleep(response.headers.get("Retry-After", 1)) # if the header retry-after is available use that, otherwhise use 0.5
